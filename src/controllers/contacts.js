9773ecf6dae07cab5148717851d2877ed39b370d
Kateryna Hizhevska <167450647+KaterynaHizhevska@users.noreply.github.com>
2025-02-24 00:50:38 +0100

    fix patch

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -41,11 +41,15 @@ export const createContactController = async (req, res, next) => {
 
 export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
-  const { contact } = await patchContact(contactId, req.body);
+  const result = await patchContact(contactId, req.body);
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
   res.json({
     status: 200,
     message: 'Successfully patched a contact!',
-    data: contact,
+    data: result.contact,
   });
 };
 
