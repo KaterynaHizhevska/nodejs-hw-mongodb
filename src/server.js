44e052bdff23bc1d93aa02138cca3cf1e63b9c63
Kateryna Hizhevska <167450647+KaterynaHizhevska@users.noreply.github.com>
2025-02-26 02:22:05 +0100

    fix import

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import dotenv from 'dotenv';
-import router from './routers/contacts.js';
+import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
