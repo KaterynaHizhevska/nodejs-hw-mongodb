06736f74c7cfa5c71559247ad483ccce7bbfc3d0
Kateryna Hizhevska <167450647+KaterynaHizhevska@users.noreply.github.com>
2025-02-26 02:40:41 +0100

    fix

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,7 +3,7 @@ import { randomBytes } from 'crypto';
 import createHttpError from 'http-errors';
 import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../constants/constants.js';
 import { UsersCollection } from '../db/models/users.js';
-import { SessionsCollection } from '../db/models/sessions.js';
+import { SessionsCollection } from '../db/models/session.js';
 
 export const registerUser = async (payload) => {
     const user = await UsersCollection.findOne({ email: payload.email });
